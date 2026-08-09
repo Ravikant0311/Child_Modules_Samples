f14a8173aadc8687cc43c2b7afe3a173b4090a98
Ravikant0311 <ravikantsharma028@gmail.com>
2026-08-09 17:36:45 +0530

    yml updated & state untracked

--- Environment/Prod/provider.tf
+++ Environment/Prod/provider.tf
@@ -6,9 +6,10 @@ terraform {
     }
   }
 }
+
 provider "azurerm" {
-  features {
+  features {}
 
-  }
-
+  subscription_id = "6f86eca6-b83f-4894-ac69-1e5893305dc2"
+  tenant_id       = "a7138af7-fb0c-43bf-81db-7f2458a2cd22"
 }
